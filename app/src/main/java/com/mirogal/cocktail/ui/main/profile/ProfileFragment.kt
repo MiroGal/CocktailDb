244c6817a00fa82b237831e04a5bba938135e8ef
MiroGal <mirogal.dev@gmail.com>
2020-07-07 02:35:17 +0300

    Fixed cut

--- app/src/main/java/com/mirogal/cocktail/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/mirogal/cocktail/ui/main/profile/ProfileFragment.kt
@@ -3,7 +3,6 @@ package com.mirogal.cocktail.ui.main.profile
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import com.mirogal.cocktail.R
@@ -28,78 +27,13 @@ class ProfileFragment : BaseFragment<ProfileViewModel>() {
 
         btn_logout.setOnClickListener {
 //            openAuthActivity()
-            cut()
+            calculateMinimumAmountOfWoodBars(900, 900, 900, 900,
+                    1275, 1275, 1275, 1275, 1275, 1275, 1275,
+                    375, 375, 375, 375, 375, 375, 375,
+                    1150, 1150, 1150, 1150, 1150,
+                    1700, 1700, 1700, 1700, 1700, 1700, 1700, 1700)
         }
     }
-
-    private fun cut() {
-
-        val stockWoodBarLength = 3000
-
-        val woodBarList = ArrayList<Int>()
-        for (i in 1..4) {
-            woodBarList.add(900)
-        }
-        for (i in 1..7) {
-            woodBarList.add(1275)
-        }
-        for (i in 1..7) {
-            woodBarList.add(375)
-        }
-        for (i in 1..5) {
-            woodBarList.add(1150)
-        }
-        for (i in 1..8) {
-            woodBarList.add(1700)
-        }
-
-        var currentWoodBarLength = stockWoodBarLength
-        var longestCut = 0
-        var wasteLength = 0
-        var totalWasteLength = 0
-        var numberWoodBar = 0
-        var counter = 0
-
-        val stringBuffer = StringBuffer()
-
-        do {
-            currentWoodBarLength = stockWoodBarLength
-            wasteLength = 0
-            counter += 1
-
-            stringBuffer.append("${counter}.\t")
-            stringBuffer.append("$currentWoodBarLength")
-            stringBuffer.append(" - ")
-            do {
-                longestCut = 0
-                for (i in woodBarList) {
-                    if (i in (longestCut + 1)..currentWoodBarLength) {
-                        longestCut = i
-                    }
-                }
-                if (longestCut != 0) {
-                    currentWoodBarLength -= longestCut
-                    woodBarList.remove(longestCut)
-                } else {
-                    wasteLength = currentWoodBarLength
-                    totalWasteLength += wasteLength
-                    numberWoodBar += 1
-                }
-
-                if (longestCut > 0) {
-                    stringBuffer.append("$longestCut / ")
-                }
-
-            } while (longestCut != 0)
-            stringBuffer.append(" west: $wasteLength\n")
-
-        } while (woodBarList.isNotEmpty())
-        stringBuffer.append("Total west: $totalWasteLength")
-
-        tv_result.text = stringBuffer
-//        Toast.makeText(requireActivity(), stringBuffer, Toast.LENGTH_LONG).show()
-    }
-
 
     private fun openAuthActivity() {
         val intent = Intent(activity, AuthActivity::class.java).apply {
@@ -108,6 +42,66 @@ class ProfileFragment : BaseFragment<ProfileViewModel>() {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
         startActivity(intent)
+    }
+
+    private fun calculateMinimumAmountOfWoodBars(vararg length: Int) {
+
+        val stockWoodBarLength = 3000
+
+        require(length.all { it < stockWoodBarLength }) { "Some wood planks segment length exceed stock bar length ($stockWoodBarLength mm)" }
+
+        val woodBarList = length.toMutableList()
+
+        var currentWoodBarNumber = 0
+        var currentWoodBarLength: Int
+        var longestSegmentLength: Int
+        var offCutLength: Int
+
+        var segmentInWoodBarLength: Int
+        var totalSegmentLength = 0
+        var totalOffCutLength = 0
+
+        val mainSb = StringBuffer()
+        val localSb = StringBuffer()
+
+        mainSb.append("Index\t| Sum\t| Off\t| Segments\n\n")
+
+        while (woodBarList.isNotEmpty()) {
+            // Оновлюю змінні для поточного бруска
+            currentWoodBarNumber += 1
+            currentWoodBarLength = stockWoodBarLength
+            segmentInWoodBarLength = 0
+            localSb.setLength(0)
+            do {
+                // Визначаю найдовший відрізок в списку коротший за поточну довжину бруска
+                longestSegmentLength = 0
+                for (i in woodBarList) {
+                    if (i in (longestSegmentLength + 1)..currentWoodBarLength) {
+                        longestSegmentLength = i
+                    }
+                }
+
+                if (longestSegmentLength != 0) {
+                    // Вкорочую брусок на визначений відрізок і видаляю цей відрізок із списку
+                    currentWoodBarLength -= longestSegmentLength
+                    woodBarList.remove(longestSegmentLength)
+                    segmentInWoodBarLength += longestSegmentLength
+
+                    localSb.append("$longestSegmentLength\t")
+                } else {
+                    // Відправляю відрізок у відходи
+                    offCutLength = currentWoodBarLength
+                    totalOffCutLength += offCutLength
+
+                    mainSb.append("${currentWoodBarNumber}.\t| $segmentInWoodBarLength\t| $offCutLength\t| " + localSb + "\n")
+                }
+            } while (longestSegmentLength != 0)
+            totalSegmentLength += segmentInWoodBarLength
+        }
+        mainSb.append("\nTotal Amount Of Wood Planks: $currentWoodBarNumber\n"
+                + "Total Segment Length: $totalSegmentLength\n"
+                + "Total Off-Cut Length: $totalOffCutLength")
+        tv_result.text = mainSb
     }
 
 }
