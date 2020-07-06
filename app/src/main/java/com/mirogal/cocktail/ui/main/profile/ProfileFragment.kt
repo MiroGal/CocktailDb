948076a0f77e74515ca19a70841bab80263b4826
MiroGal <mirogal.dev@gmail.com>
2020-07-06 16:03:55 +0300

    Cut method

--- app/src/main/java/com/mirogal/cocktail/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/mirogal/cocktail/ui/main/profile/ProfileFragment.kt
@@ -3,6 +3,7 @@ package com.mirogal.cocktail.ui.main.profile
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
 import com.mirogal.cocktail.R
@@ -25,7 +26,78 @@ class ProfileFragment : BaseFragment<ProfileViewModel>() {
         (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
         (activity as AppCompatActivity).supportActionBar?.setTitle(R.string.profile_label)
 
-        btn_logout.setOnClickListener { openAuthActivity() }
+        btn_logout.setOnClickListener {
+//            openAuthActivity()
+            cut()
+        }
+    }
+
+    private fun cut() {
+
+        val stockWoodBarLength = 3000
+
+        val woodBarList = ArrayList<Int>()
+        for (i in 1..4) {
+            woodBarList.add(900)
+        }
+        for (i in 1..7) {
+            woodBarList.add(1275)
+        }
+        for (i in 1..7) {
+            woodBarList.add(375)
+        }
+        for (i in 1..5) {
+            woodBarList.add(1150)
+        }
+        for (i in 1..8) {
+            woodBarList.add(1700)
+        }
+
+        var currentWoodBarLength = stockWoodBarLength
+        var longestCut = 0
+        var wasteLength = 0
+        var totalWasteLength = 0
+        var numberWoodBar = 0
+        var counter = 0
+
+        val stringBuffer = StringBuffer()
+
+        do {
+            currentWoodBarLength = stockWoodBarLength
+            wasteLength = 0
+            counter += 1
+
+            stringBuffer.append("${counter}.\t")
+            stringBuffer.append("$currentWoodBarLength")
+            stringBuffer.append(" - ")
+            do {
+                longestCut = 0
+                for (i in woodBarList) {
+                    if (i in (longestCut + 1)..currentWoodBarLength) {
+                        longestCut = i
+                    }
+                }
+                if (longestCut != 0) {
+                    currentWoodBarLength -= longestCut
+                    woodBarList.remove(longestCut)
+                } else {
+                    wasteLength = currentWoodBarLength
+                    totalWasteLength += wasteLength
+                    numberWoodBar += 1
+                }
+
+                if (longestCut > 0) {
+                    stringBuffer.append("$longestCut / ")
+                }
+
+            } while (longestCut != 0)
+            stringBuffer.append(" west: $wasteLength\n")
+
+        } while (woodBarList.isNotEmpty())
+        stringBuffer.append("Total west: $totalWasteLength")
+
+        tv_result.text = stringBuffer
+//        Toast.makeText(requireActivity(), stringBuffer, Toast.LENGTH_LONG).show()
     }
 
 
