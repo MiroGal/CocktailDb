6b391ec0ee2c97ec626a51b9733dc8f5f5c1b871
MiroGal <mirogal.dev@gmail.com>
2020-07-19 03:24:33 +0300

    Fixed searchView

--- app/src/main/java/com/mirogal/cocktail/presentation/ui/search/SearchActivity.kt
+++ app/src/main/java/com/mirogal/cocktail/presentation/ui/search/SearchActivity.kt
@@ -5,6 +5,7 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.view.Menu
 import android.view.View
+import android.widget.LinearLayout
 import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
