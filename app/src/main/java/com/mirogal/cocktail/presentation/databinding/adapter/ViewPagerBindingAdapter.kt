83e548588effe11d973b5d437cfa0b0ff059529a
MiroGal <mirogal.dev@gmail.com>
2020-08-28 03:20:35 +0300

    Не працює двохсторонній dataBinding: fragment_drink_pager.xml ViewPager2

--- app/src/main/java/com/mirogal/cocktail/presentation/databinding/adapter/ViewPagerBindingAdapter.kt
+++ app/src/main/java/com/mirogal/cocktail/presentation/databinding/adapter/ViewPagerBindingAdapter.kt
@@ -2,12 +2,13 @@ package com.mirogal.cocktail.presentation.databinding.adapter
 
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import androidx.lifecycle.MutableLiveData
 import androidx.viewpager2.widget.ViewPager2
 
 object ViewPagerBindingAdapter {
 
-    @BindingAdapter("android:bind_vp_currentPage")
+    @BindingAdapter("android:bind_vp_page")
     @JvmStatic
     fun ViewPager2.setPage(pageLiveData: MutableLiveData<Int?>) {
         pageLiveData.value?.let {
@@ -17,8 +18,14 @@ object ViewPagerBindingAdapter {
         }
     }
 
-    @InverseBindingAdapter(attribute = "android:bind_vp_currentPage", event = "android:bind_vp_currentPageAttrChanged")
+    @InverseBindingAdapter(attribute = "android:bind_vp_page", event = "android:bind_vp_pageAttrChanged")
     @JvmStatic
     fun ViewPager2.getPage() = currentItem
+
+    @BindingAdapter("android:bind_vp_pageAttrChanged")
+    @JvmStatic
+    fun ViewPager2.pageChangeListener(pageAttrChanged: InverseBindingListener?) {
+
+    }
 
 }
